b9884c44e7a8c80b88c786a4f33c3f12099c3479
sukeerti <sukeerthi.botu@gmail.com>
2020-09-15 16:07:03 +0530

    feat(bookmark): add basic open api documentation

--- rest-adapter/src/main/java/com/bookmark/rest/BookmarkController.java
+++ rest-adapter/src/main/java/com/bookmark/rest/BookmarkController.java
@@ -3,7 +3,9 @@ package com.bookmark.rest;
 import static com.bookmark.rest.mapper.BookmarkMapper.BOOKMARK_MAPPER;
 import com.bookmark.rest.model.Bookmark;
 import com.bookmark.rest.model.BookmarkDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +20,44 @@ import java.net.URI;
 @CrossOrigin(origins = "http://localhost:4200")
 public class BookmarkController {
 
-	@Autowired
 	private RequestBookmark requestBookmark;
 
+	public BookmarkController(final RequestBookmark requestBookmark) {
+		this.requestBookmark = requestBookmark;
+	}
 
+	@Operation(summary = "Get list of bookmarks")
+	@ApiResponse(responseCode = "200", description = "Successfully retrieved the bookmarks")
+	@ApiResponse(responseCode = "404", description = "Service not available")
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<BookmarkDetails> getBookmarks() {
 		return ResponseEntity.ok(BookmarkDetails.builder().bookmarks(BOOKMARK_MAPPER
 				.constructBookmark(requestBookmark.getBookmarks())).build());
 	}
 
+	@Operation(summary = "Get list of bookmarks by group")
+	@ApiResponse(responseCode = "200", description = "Successfully retrieved the bookmarks for given group")
+	@ApiResponse(responseCode = "404", description = "Service not available")
 	@GetMapping(value = "/{groupId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<BookmarkDetails> getBookmarks(@PathVariable Long groupId) {
+	public ResponseEntity<BookmarkDetails> getBookmarks(@Parameter(description = "id of the group to get bookmarks") @PathVariable Long groupId) {
 		return ResponseEntity.ok(BookmarkDetails.builder().bookmarks(BOOKMARK_MAPPER
 				.constructBookmark(requestBookmark.getBookmarks(groupId))).build());
 	}
 
+	@Operation(summary = "Create a new bookmark")
+	@ApiResponse(responseCode = "200", description = "Successfully created the bookmark")
+	@ApiResponse(responseCode = "404", description = "Service not available")
 	@PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity createBookmark(@RequestBody Bookmark bookmark) {
 		requestBookmark.save(BOOKMARK_MAPPER.constructBookmark(bookmark));
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "Redirect to original url")
+	@ApiResponse(responseCode = "200", description = "Successfully redirected to the original url")
+	@ApiResponse(responseCode = "404", description = "Service not available")
 	@GetMapping(value = "/{shortUrl}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Void> redirect(@PathVariable String shortUrl) {
+	public ResponseEntity<Void> redirect(@Parameter(description = "short url") @PathVariable String shortUrl) {
 		String url = requestBookmark.getOriginalURL(shortUrl);
 		return ResponseEntity.status(HttpStatus.FOUND)
 				.location(URI.create(url))
--- rest-adapter/src/main/java/com/bookmark/rest/GroupController.java
+++ rest-adapter/src/main/java/com/bookmark/rest/GroupController.java
@@ -5,7 +5,8 @@ import static com.bookmark.rest.mapper.GroupMapper.GROUP_MAPPER;
 import com.bookmark.domain.port.RequestGroup;
 import com.bookmark.rest.model.Group;
 import com.bookmark.rest.model.GroupDetails;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +16,27 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class GroupController {
 
-    @Autowired
     private RequestGroup requestGroup;
 
+    public GroupController(final RequestGroup requestGroup) {
+        this.requestGroup = requestGroup;
+    }
+
+    @Operation(summary = "Create a new group")
+    @ApiResponse(responseCode = "200", description = "Successfully created the groups")
+    @ApiResponse(responseCode = "404", description = "Service not available")
     @PostMapping(value = "/groups", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createBookmark(@RequestBody Group group) {
         requestGroup.save(GROUP_MAPPER.constructGroup(group));
         return ResponseEntity.ok().build();
     }
 
+
+    @Operation(summary = "Get list of groups")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved all groups")
+    @ApiResponse(responseCode = "404", description = "Service not available")
     @GetMapping(value = "/groups", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GroupDetails> getBookmarks() {
+    public ResponseEntity<GroupDetails> getGroups() {
         return ResponseEntity.ok(GroupDetails.builder().groups(GROUP_MAPPER.constructGroup(requestGroup.getGroups())).build());
     }
 }
