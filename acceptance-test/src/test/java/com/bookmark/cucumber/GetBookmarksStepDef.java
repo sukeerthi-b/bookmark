453b32ca60b99a4dbc2dc4add03039827193fb45
sukeerti <sukeerthi.botu@gmail.com>
2020-09-16 10:11:13 +0530

    feat(bookmark): add get bookmarks feature file and test cases

--- acceptance-test/src/test/java/com/bookmark/cucumber/GetBookmarksStepDef.java
+++ acceptance-test/src/test/java/com/bookmark/cucumber/GetBookmarksStepDef.java
@@ -1,10 +1,16 @@
 package com.bookmark.cucumber;
 
-//import cucumber.api.java8.En;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 import com.bookmark.BookmarkE2EApplication;
-import com.bookmark.domain.model.Bookmark;
 import com.bookmark.jpa.dao.BookmarkDao;
+import com.bookmark.jpa.entity.BookmarkEntity;
+import com.bookmark.rest.model.Bookmark;
+import com.bookmark.rest.model.BookmarkDetails;
+import cucumber.api.DataTable;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,11 +18,14 @@ import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import cucumber.api.java8.En;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 
@@ -25,22 +34,43 @@ import java.util.List;
 @ContextConfiguration(classes = BookmarkE2EApplication.class, loader = SpringBootContextLoader.class)
 @TestPropertySource(locations = "classpath:application-test.yml")
 @ActiveProfiles("test")
-public class GetBookmarksStepDef {
+public class GetBookmarksStepDef implements En{
     private static final String LOCALHOST = "http://localhost:";
     private static final String API_URL = "api/v1/bookmarks";
     private BookmarkDao bookmarkDao;
     private TestRestTemplate restTemplate;
     @LocalServerPort
     private int port;
-    private ResponseEntity<List<Bookmark>> bookmarkResponse;
+    private ResponseEntity<BookmarkDetails> responseEntity;
 
     public GetBookmarksStepDef(TestRestTemplate template, BookmarkDao bookmarkDao) {
         this.restTemplate = template;
         this.bookmarkDao = bookmarkDao;
 
-//        Given("^the following bookmark information", (DataTable privateListDataTable) -> {
-//
-//        });
+        Given("^the following bookmark information", (DataTable bookmarkDataTable) -> {
+            List<BookmarkEntity> bookmarkEntities = bookmarkDataTable.asList(BookmarkEntity.class);
+            bookmarkDao.saveAll(bookmarkEntities);
+        });
+
+        When("user requests for bookmark details", () -> {
+            String url = LOCALHOST + port + "/api/v1/bookmarks";
+            responseEntity = restTemplate.exchange(url, HttpMethod.GET, generateHeader(), BookmarkDetails.class);
+        });
+
+        Then("user gets bookmark information as", (DataTable response) ->{
+            List<Bookmark> bookmarks = response.asList(Bookmark.class);
+            List<Bookmark> bookmarksFormResponse = responseEntity.getBody().getBookmarks();
+            assertThat(responseEntity).extracting("status").isEqualTo(HttpStatus.OK.value());
+            assertThat(bookmarksFormResponse).extracting("title", "source")
+                    .contains(tuple(bookmarks.get(0).getTitle(), bookmarks.get(0).getSource()));
+        });
+    }
+
+    HttpEntity<MultiValueMap<String, String>> generateHeader()  {
+        HttpHeaders headers = new HttpHeaders();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(map, headers);
+        return request;
     }
 
     private void dbCleanUp() {
--- acceptance-test/src/test/resources/features/get-bookmarks.feature
+++ acceptance-test/src/test/resources/features/get-bookmarks.feature
@@ -2,11 +2,11 @@ Feature: User would like to get the bookmarks
 
   Background:
     Given the following bookmark information
-      | bookmrak_tecid | actual_url         | title  | source | short_url     |
-      | 1              | https://google.com | google | SG     | https://g.com |
+      | createdBy | actualUrl         | title  | source |
+      | sukeerti  | https://google.com| google | SG     |
 
   Scenario: User request for bookmark information
     When user requests for bookmark details
     Then user gets bookmark information as
-      | bookmrak_tecid | actual_url         | title  | source | short_url     |
-      | 1              | https://google.com | google | SG     | https://g.com |
+      | createdBy | actualUrl         | title  | source | short_url     |
+      | sukeerti  | https://google.com| google | SG     | https://g.com |
